514df04ff501374c95d2ab1d0a16eaca50b93baa
Gaurav Chandiramani <47972788+gauravc6@users.noreply.github.com>
2019-09-15 15:20:14 +0530

    Added model for user

---
+++ malarianet/models.py
@@ -0,0 +1,27 @@
+from malarianet import db, login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from datetime import datetime
+
+
+class User(db.Model,UserMixin):
+
+    __tablname__ = 'users'
+
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(64),unique=True,index=True)
+    email = db.Column(db.String(64),unique=True,index=True)
+    password_hash = db.Column(db.String(128))
+
+    timeline = db.relationship('timeline',backref='user',lazy=True)
+
+    def __init__(self,email,username,password):
+        self.email = email
+        self.username = username
+        self.password_has = generate_password_hash(password)
+
+    def check_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
+    def __repr__(self):
+        return f"Username: {self.username}"
