b0276711222988f21260169f0ab449fe1b5ef681
Gaurav Chandiramani <47972788+gauravc6@users.noreply.github.com>
2019-09-29 16:47:31 +0530

    Updated model function

--- malarianet/predict/model_handler.py
+++ malarianet/predict/model_handler.py
@@ -1,12 +1,14 @@
-from keras.models import load_model
-from PIL import Image
 import cv2
+import numpy as np
+from PIL import Image
+from keras.models import load_model
 
-def loadmodel():
+def preprocess(image):
+    #return preprocessed Image
     pass
 
-def preprocess():
-    pass
 
-def final_predict():
-    pass
+def final_predict(image):
+    model = load_model('model.h5')
+    prediction = model.predict_classes(image)
+    return prediction
