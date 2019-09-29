d364a1660647a64081f69a3e0f8ea22116cef71b
Gaurav Chandiramani <47972788+gauravc6@users.noreply.github.com>
2019-09-29 16:44:44 +0530

    Added routes for result and upload

--- malarianet/predict/views.py
+++ malarianet/predict/views.py
@@ -1,4 +1,19 @@
 from flask import Blueprints, render_template
 from malarianet.predict.forms import UploadImage
+from model_handler import loadmodel, final_predict, preprocess
 
 predict = Blueprint("predict",__name__)
+
+@predict.route('/upload',methods=['GET','POST'])
+def upload():
+    form = UploadImage()
+
+    if form.validate_on_submit():
+        global image = preprocess(form.picture.data)
+        return redirect(url_for('predict.result'))
+    return render_template('upload.html')
+
+@def.route('/result',methods=['GET','POST'])
+def result():
+    result = final_predict(image)
+    render_template('result.html',result=result)
