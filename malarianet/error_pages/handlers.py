3103decae4f5c82dc1c4ca49f9b08cc5a63f731f
Gaurav Chandiramani <47972788+gauravc6@users.noreply.github.com>
2019-10-07 16:36:33 +0530

    Added 401 error page

--- malarianet/error_pages/handlers.py
+++ malarianet/error_pages/handlers.py
@@ -7,7 +7,10 @@ def error_404(error):
     return render_template('error_pages/404.html') , 404
 
 
-@ error_pages.app_errorhandler(403)
+@error_pages.app_errorhandler(403)
 def error_403(error):
     return render_template('error_pages/403.html') , 403
-    
+
+@error_pages.app_errorhandler(401)
+def error_401(error):
+    return render_template('error_pages/401.html') , 401
