7c031f386903ee36b1d6e3323c2f4a02df7429e6
Gaurav Chandiramani <47972788+gauravc6@users.noreply.github.com>
2019-10-20 23:20:15 +0530

    Few changes

--- malarianet/predict/views.py
+++ malarianet/predict/views.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template
+import cv2
+from keras import backend as k
+from flask import Blueprint, render_template, redirect,url_for
 from flask_login import login_required
+from keras.models import load_model
 from malarianet.predict.forms import UploadImage
-from malarianet.predict.model_handler import final_predict, preprocess
+from malarianet.predict.image_handler import preprocess
 
 predict = Blueprint("predict",__name__)
 
@@ -11,15 +14,19 @@ def upload():
     form = UploadImage()
 
     if form.validate_on_submit():
-        image = preprocess(form.picture.data)
+        global image, img_data
+        img_data = form.picture.data
+        image = preprocess(img_data)
         return redirect(url_for('predict.result'))
-    return render_template('upload.html')
+    return render_template('upload.html',form=form)
 
 @predict.route('/result',methods=['GET','POST'])
 def result():
-    if upload.image:
-        result = final_predict(upload.image)
+    k.clear_session()
+    model = load_model('model.h5')
+    if image.shape!=():
+        result = model.predict_classes(image)
     else:
-        redirect(url_for('predict.upload'))
+        return redirect(url_for('predict.upload'))
 
-    render_template('result.html',result=result)
+    return render_template('result.html',result=result)
