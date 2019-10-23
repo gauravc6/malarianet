4c3338bc276a0ebaeacadc83f9a0f21a54b60932
Gaurav Chandiramani <47972788+gauravc6@users.noreply.github.com>
2019-10-23 20:19:31 +0530

    minor improvements

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
+import warnings
 from malarianet import app
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        warnings.warn("deprecated", DeprecationWarning)
